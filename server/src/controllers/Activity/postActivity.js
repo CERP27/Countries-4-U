b23f418ba5a86a0e529fae09ecb51a7c17d36ee9
ElectroCERP <carloscer27r@gmail.com>
2023-06-05 20:42:22 -0300

    Rutas hasta el getByName

--- server/src/controllers/Activity/postActivity.js
+++ server/src/controllers/Activity/postActivity.js
@@ -1,10 +1,25 @@
+const {Activity} = require('../../db')
 
 const postActivity = async(
     name,dificulty,duration,season,countries
 ) => {
     if(!countries) throw Error('You Must Provide a Country')
 
+    const countryList = Array.isArray(countries) ? countries : [countries];
+
+    if(![name,dificulty,duration,season].every(Boolean)) throw Error('Missing data')
     
+    countryList.forEach((id))
+
+    Activity.findOrCreate({where:{
+        name,
+        dificulty,
+        season,
+        duration
+    }})
+
+
+
 }
 
 module.exports = {
