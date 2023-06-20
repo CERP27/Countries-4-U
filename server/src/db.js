c103f65e364ff2cc8a2649ac85df18fd9d9fa225
ElectroCERP <carloscer27r@gmail.com>
2023-06-20 19:56:24 -0300

    PI no deploy

--- server/src/db.js
+++ server/src/db.js
@@ -7,19 +7,19 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-//   {
-//   logging: false, 
-//   native: false, 
-// });
-
 const sequelize = new Sequelize(
-  DB_DEPLOY,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
   {
   logging: false, 
   native: false, 
 });
+
+// const sequelize = new Sequelize(
+//   DB_DEPLOY,
+//   {
+//   logging: false, 
+//   native: false, 
+// });
 
 const basename = path.basename(__filename);
 
